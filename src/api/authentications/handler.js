d76c41aa75f69d8557aac73ec5d107418d52e977
KevinSinatria <kevinsinatria8@gmail.com>
2025-08-07 18:06:18 +0700

    chore:add 401, 403 exception

--- src/api/authentications/handler.js
+++ src/api/authentications/handler.js
@@ -1,8 +1,12 @@
 class AuthenticationsHandler {
-   constructor(authenticationService, usersService, tokenManager, validator) {
-      this._authenticationService = authenticationService;
-      this._usersService = usersService;
-      this._tokenManager = tokenManager;
-      this._validator = validator;
+	constructor(authenticationService, usersService, tokenManager, validator) {
+		this._authenticationService = authenticationService;
+		this._usersService = usersService;
+		this._tokenManager = tokenManager;
+		this._validator = validator;
+	}
+
+   async postAuthenticationHandler (request, h) {
+      this._validator.valida
    }
 }
--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -2,6 +2,7 @@ const { Pool } = require("pg");
 const InvariantError = require("../../exceptions/InvariantError");
 const { nanoid } = require("nanoid");
 const bcrypt = require("bcrypt");
+const NotFoundError = require("../../exceptions/NotFoundError");
 
 class UsersService {
 	constructor() {
@@ -41,6 +42,43 @@ class UsersService {
 				"Gagal menambahkan user. Username sudah digunakan."
 			);
 		}
+	}
+
+	async getUserById(userId) {
+		const query = {
+			text: "SELECT id, username, fullname FROM users WHERE id = $1",
+			values: [userId],
+		};
+
+		const result = await this._pool.query(query);
+
+		if (!result.rows.length) {
+			throw new NotFoundError("User tidak ditemukan");
+		}
+
+		return result.rows[0];
+	}
+
+	async verifyUserCredential(username, password) {
+		const query = {
+			text: "SELECT id, password FROM users WHERE username = $1",
+			values: [username],
+		};
+
+		const result = await this._pool.query(query);
+
+		if (!result.rows.length) {
+			throw new AuthenticationError("Kredensial yang Anda berikan salah");
+		}
+
+		const { id, password: hashedPassword } = result.rows[0];
+		const match = await bcrypt.compare(password, hashedPassword);
+
+		if (!match) {
+			throw new AuthenticationError("Kredensial yang Anda berikan salah");
+		}
+
+		return id;
 	}
 }
 
